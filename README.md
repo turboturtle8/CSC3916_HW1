45e808d41f0349fb8fb3d51e3aae71b3be3116b2
turboturtle8 <31858764+turboturtle8@users.noreply.github.com>
2021-02-13 23:02:11 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # csc3916_hw1
 
 
-[![Run in Postman](https://run.pstmn.io/button.svg)](https://app.getpostman.com/run-collection/07950ff2e9ec2dcbb912)
--- server.js
+++ server.js
@@ -6,23 +6,56 @@ server.on("request", (request, response) => {
     request.on("data", chunk => {
         body.push(chunk);
     });
+
     request
         .on("end", () => {
-            body = body.concat.toString();  //end request is one per request
-            response.end(body);
+            let bodyString = body.concat().toString();
+            console.log(bodyString);
+            response.end(bodyString);
         })
         .on("error", () => {
             response.statusCode = 400;
             response.end();
         });
     response.on("error", err => {
-        console.err(err);
+        console.error(err);
     });
-    //response.write("Hello 2021 World!");
-    //response.end();
-    console.log(sever.address());
 });
-
 server.listen(process.env.PORT || 8008, () => {
     console.log("Server listening at 8008");
 });
+//    request
+//        .on("end", () => {
+//            body = body.concat.toString();  //end request is one per request
+//            response.end(body);
+//        })
+//        .on("error", () => {
+//            response.statusCode = 400;
+//            response.end();
+//        });
+//    response.on("error", err => {
+//        console.err(err);
+//    });
+
+//    request
+//        .on("end", () => {
+//            let bodyString = body.concat().toString();
+//            console.log(bodyString);
+//            response.end(bodyString);
+//        })
+//        .on("error", () => {
+//            response.statusCode = 400;
+//            response.end();
+//        });
+//    response.on("error", err => {
+//        console.error(err);
+//    });
+//});
+//    //response.write("Hello 2021 World!");
+//    //response.end();
+//    console.log(sever.address());
+//});
+
+//server.listen(process.env.PORT || 8008, () => {
+//    console.log("Server listening at 8008");
+//});
