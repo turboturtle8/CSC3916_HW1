c954b716fe1f5600936b2310ee0ea26f10252920
turboturtle8 <31858764+turboturtle8@users.noreply.github.com>
2021-02-14 12:26:13 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,29 +1,45 @@
-var http = reqire("http");
-var server = http.createServer();
-server.on("request", (request, response) => {
-    //stuff goes here
-    var body = [];
-    request.on("data", chunk => {
-        body.push(chunk);
-    });
+const http = require('http')
 
-    request
-        .on("end", () => {
-            let bodyString = body.concat().toString();
-            console.log(bodyString);
-            response.end(bodyString);
-        })
-        .on("error", () => {
-            response.statusCode = 400;
-            response.end();
-        });
-    response.on("error", err => {
-        console.error(err);
-    });
-});
-server.listen(process.env.PORT || 8008, () => {
-    console.log("Server listening at 8008");
-});
+const app = http.createServer((request, response) => {
+    response.writeHead(200, { 'Content-Type': 'text/plain' })
+    response.end('Hello World')
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
+
+
+
+
+/**********************A million different attempts****************************/
+//var http = reqire("http");
+//var server = http.createServer();
+//server.on("request", (request, response) => {
+//    //stuff goes here
+//    var body = [];
+//    request.on("data", chunk => {
+//        body.push(chunk);
+//    });
+
+//    request
+//        .on("end", () => {
+//            //let bodyString = body.concat().toString();
+//            //console.log(bodyString);
+//            //response.end(bodyString);
+//            response.end('hello world 1222');
+//        })
+//        .on("error", () => {
+//            response.statusCode = 400;
+//            response.end();
+//        });
+//    response.on("error", err => {
+//        console.error(err);
+//    });
+//});
+//server.listen(process.env.PORT || 8008, () => {
+//    console.log("Server listening at 8008");
+//});
 //    request
 //        .on("end", () => {
 //            body = body.concat.toString();  //end request is one per request
